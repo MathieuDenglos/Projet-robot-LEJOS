11032cdca248e53d92457c08e82e9259a497c116
Mathieu Denglos <matdenglos@gmail.com>
2021-01-27 19:49:55 +0100

    update visite_couloir

--- Couloir.java
+++ Couloir.java
@@ -12,42 +12,41 @@ public class Couloir {
         return this.orientation == robot.get_orientation();
     }
 
-    public void visite_couloir(Robot robot)
-	{
-		//fait avancer le robot jusqu'au prochain noeud
-        robot.avancer();
+    public void visite_couloir(Robot robot) {
+        // oriente le robot et le fait avancer jusqu'au prochain noeud
+        robot.avancer_au_noeud(this.orientation);
 
-        //récupère le noeud avec sa couleur et ses potentiels chemins
+        // récupère le noeud avec sa couleur et ses potentiels chemins
         noeud = robot.scan();
 
-        //Si le noeud est un carrefour, analyse le carrefour pour de potentiel chemins à emprunter
-        if(noeud.get_couleur() == TypeNoeud.embranchement)
-        {        
-            //explore tous les embranchements et sous-embranchements du noeud
-            for(Couloir couloir : noeud.get_couloirs())
-            {
+        // Si le noeud est un carrefour, analyse le carrefour pour de potentiel chemins
+        // à emprunter
+        if (noeud.get_couleur() == TypeNoeud.embranchement) {
+            // explore tous les embranchements et sous-embranchements du noeud
+            for (Couloir couloir : noeud.get_couloirs()) {
                 couloir.visite_couloir(robot);
                 System.out.println("parcours d'un chemin, " + couloir.orientation.toString());
-                if(robot.get_tresor_trouve())
-                {
-                    robot.demi_tour(couloir.orientation);
+                if (robot.get_tresor_trouve()) {
+                    robot.avancer_au_noeud(this.orientation.droite().droite());
                     return;
-                } 
+                }
             }
+            robot.avancer_au_noeud(this.orientation.droite().droite());
         }
-        //Si le noeud contient le trésor, indique au robot que le trésor a été trouvé et retourne
-        else if(noeud.get_couleur() == TypeNoeud.tresor)
-        {
+        // Si le noeud contient le trésor, indique au robot que le trésor a été trouvé
+        // et retourne
+        else if (noeud.get_couleur() == TypeNoeud.tresor) {
             System.out.println("tresor récupéré, retour au départ");
             robot.set_tresor_trouve(true);
-            robot.demi_tour(couloir.orientation);
+            robot.avancer_au_noeud(this.orientation.droite().droite());
             return;
         }
-        //Si le noeud est un cul de sac ou qu'aucune des branches ne mène au trésor, fait demi tour
-        else(noeud.get_couleur() == TypeNoeud.cul_de_sac)
-        {
+        // Si le noeud est un cul de sac ou qu'aucune des branches ne mène au trésor,
+        // fait demi tour
+        else if (noeud.get_couleur() == TypeNoeud.cul_de_sac) {
             System.out.println("cul_de_sac : demi tour");
-            robot.demi_tour(this.orientation);
+            robot.avancer_au_noeud(this.orientation.droite().droite());
+            return;
         }
-	}
+    }
 }
