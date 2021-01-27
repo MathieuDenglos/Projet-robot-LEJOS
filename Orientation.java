6b5bdd807ac792cf9aab5c1f39d77b0e94506e1a
Mathieu Denglos <matdenglos@gmail.com>
2021-01-27 16:59:03 +0100

    add difference method in Orientation to compare 2 orientations

--- Orientation.java
+++ Orientation.java
@@ -10,4 +10,14 @@ public enum Orientation {
     public final Orientation gauche() {
         return valeurs[(this.ordinal() - 1 + valeurs.length) % valeurs.length];
     }
+
+    public final int difference(Orientation comparaison) {
+        int temp = this.ordinal() - comparaison.ordinal();
+        if (temp == -3)
+            return 1;
+        if (temp == 3)
+            return -1;
+        else
+            return temp;
+    }
 }
