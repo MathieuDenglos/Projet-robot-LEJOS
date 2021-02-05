41ef830daa02be9d9a795a378106999bc3fbbaad
Mathieu Denglos <matdenglos@gmail.com>
2021-02-05 22:03:58 +0100

    implement functional main

--- src/Labyrinthe.java
+++ src/Labyrinthe.java
@@ -1,7 +1,12 @@
 public class Labyrinthe {
     public static void main(String[] args) {
-        Orientation test = Orientation.NORD;
-        test.droite();
-        System.out.println(test.droite().droite());
+        Robot robot = new Robot();
+        robot.calibration();
+        Couloir couloir = new Couloir(Orientation.NORD);
+        couloir.visite_couloir(robot);
+        if (robot.get_tresor_trouve()) {
+            robot.tourner(360);
+            robot.tourner(-360);
+        }
     }
 }
